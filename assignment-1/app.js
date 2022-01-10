b107bcc51e21abd6bf32eddbfa9ab72bbac93d61
Devam Parikh <devamparikh15@gmail.com>
2022-01-10 17:56:51 +0530

    filter applied to responce

--- assignment-1/app.js
+++ assignment-1/app.js
@@ -11,7 +11,24 @@ const options = {
 function callback(error, response, body) {
   if (!error && response.statusCode == 200) {
     // const info = JSON.parse(body);
-    console.log(response.body.items[0]);
+
+    response.body.items.forEach(item => {
+
+      if (item.language === 'Python' && item.forks_count >= 200 ) {
+        console.log('----------------------------------');
+        console.log('Name: ' + item.name);
+        console.log('Description: ' + item.description);
+        console.log('Html Url: ' + item.html_url);
+        console.log('Watchers Count: ' + item.watchers_count);
+        console.log('Stargazers Count: ' + item.stargazers_count);
+        console.log('Forks Count: ' + item.forks_count);
+      }else{
+        console.log('No such item with language python and forks count above 200 is found.')
+      }
+      
+    });
+
+    // console.log(response.body.items[0]);
     // console.log(info.forks_count + " Forks");
   }else{
     console.log(error);
