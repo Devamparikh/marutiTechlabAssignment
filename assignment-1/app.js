343560ba2780a20399aa9703032c32c080eeeb69
Devam Parikh <devamparikh15@gmail.com>
2022-01-10 18:42:29 +0530

    WIP

--- assignment-1/app.js
+++ assignment-1/app.js
@@ -1,4 +1,6 @@
 const request = require('request');
+const csv = require('csv');
+var obj = csv()
  
 const options = {
   url: 'https://api.github.com/search/repositories?q=is:public',
@@ -11,10 +13,10 @@ const options = {
 function callback(error, response, body) {
   if (!error && response.statusCode == 200) {
     // const info = JSON.parse(body);
-
+    let data = [];
     response.body.items.forEach(item => {
 
-      if (item.language === 'Python' && item.forks_count >= 200 ) {
+      if (item.language === 'JavaScript' && item.forks_count >= 100 ) {
         console.log('----------------------------------');
         console.log('Name: ' + item.name);
         console.log('Description: ' + item.description);
@@ -22,12 +24,29 @@ function callback(error, response, body) {
         console.log('Watchers Count: ' + item.watchers_count);
         console.log('Stargazers Count: ' + item.stargazers_count);
         console.log('Forks Count: ' + item.forks_count);
+
+        if (item.stargazers_count > 500) {
+          data.push('|')
+          data.push(item.name)
+          data.push(item.description)
+          data.push(item.html_url)
+          data.push(item.watchers_count)
+          data.push(item.stargazers_count)
+          data.push(item.forks_count)
+        }else{
+          console.log('')
+        }
       }else{
         console.log('No such item with language python and forks count above 200 is found.')
       }
       
     });
-
+    console.log(data)
+    if (data.length > 0) {
+      obj.from.array(data).to.path('./dataInfo.csv');
+    }else{
+      console.log('not stored.')
+    }
     // console.log(response.body.items[0]);
     // console.log(info.forks_count + " Forks");
   }else{
