6f4eb26d932fb06fb7254b84046cd01fd84443fe
Devamparikh <devamparikh15@gmail.com>
2022-01-11 12:25:56 +0530

    assignment complete with feature to store data in csv file

--- assignment-1/README.md
+++ assignment-1/README.md
@@ -1,1 +1,8 @@
-with all the work, that includes rough work and commenting previous code
+Here we have a github api url and from that url we want to fetch data from that api.
+without authantication git provides 30 set of data.
+
+to access this data first install npm package using npm install which will install request and csv-writer module.
+request will help to fetch api data and show json data as responce.
+and csv will be use to store that data to csv file such as dataInfo.csv
+
+foreach is used to loop through array of items in responce
--- assignment-1/app.js
+++ assignment-1/app.js
@@ -1,18 +1,19 @@
 // required modules used in assignment
 const request = require('request');
-// const csv = require('csv');
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
-// var fs = require('fs');
-// var stringify = require('csv-stringify');
- 
+
+// creating options variable as object to pass url to request function
 const options = {
   url: 'https://api.github.com/search/repositories?q=is:public',
+  //specification of user agent required for accessing github api
   headers: {
     'User-Agent': 'request'
   },
+  // get responce in json formate
   json: true
 };
 
+// create object to store value in csv file (creating csv writer)
 const csvWriter = createCsvWriter({
   path: './dataInfo.csv',
   header: [
@@ -22,17 +23,20 @@ const csvWriter = createCsvWriter({
       {id: 'watchers_count', title: 'Watchers Count'},
       {id: 'stargazers_count stargazers_count', title: 'Stargazers Count'},
       {id: 'forks_count', title: 'Forks Count'}
-
     ]
 });
  
-function callback(error, response, body) {
+
+// creating callback for request function
+function callback(error, response) {
+  // check if api got response or not
   if (!error && response.statusCode == 200) {
-    // const info = JSON.parse(body);
+    // defining data variable to store each value in array
     let data = [];
     response.body.items.forEach(item => {
-
-      if (item.language === 'JavaScript' && item.forks_count >= 100 ) {
+      // check if filter applyed 
+      if (item.language === 'Python' && item.forks_count >= 200 ) {
+        // printing data for viewing
         console.log('----------------------------------');
         console.log('Name: ' + item.name);
         console.log('Description: ' + item.description);
@@ -41,46 +45,31 @@ function callback(error, response, body) {
         console.log('Stargazers Count: ' + item.stargazers_count);
         console.log('Forks Count: ' + item.forks_count);
 
-        if (item.stargazers_count > 500) {
-
-          // data.push('|')
-          // data.push(item.name)
-          // data.push(item.description)
-          // data.push(item.html_url)
-          // data.push(item.watchers_count)
-          // data.push(item.stargazers_count)
-          // data.push(item.forks_count)
-
+        if (item.stargazers_count > 2000) {
+          //applying filter and storing data in array
           data2dArray = {name: item.name,lang: item.language, description: item.description, html_url: item.html_url, watchers_count: item.watchers_count, stargazers_count: item.stargazers_count, forks_count: item.forks_count}
           data.push(data2dArray)
           
-        }else{
-          console.log('')
         }
-      }else{
-        console.log('No such item with language python and forks count above 200 is found.')
       }
       
     });
 
-
-    console.log(data)
+    // to view data stored in csv form console uncomment below line
+    // console.log(data)
     if (data.length > 0) {
-
+      // writing in csv file
       csvWriter.writeRecords(data)       // returns a promise
     .then(() => {
         console.log('...Done');
     });
 
-      // obj.from.array(data).to.path('./dataInfo.csv');
     }else{
-      console.log('not stored.')
+      console.log('No data to store.')
     }
-    // console.log(response.body.items[0]);
-    // console.log(info.forks_count + " Forks");
   }else{
-    console.log(error);
+    console.log('API call fail due to: ' + error);
   }
 }
- 
+// request for api call
 request(options, callback);
