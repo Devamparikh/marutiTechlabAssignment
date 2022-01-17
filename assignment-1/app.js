e41d946b5f626b28b2565bb2b0d95d7424019d91
Devamparikh <devamparikh15@gmail.com>
2022-01-17 14:44:19 +0530

    yargs added for arguments to pass

--- assignment-1/app.js
+++ assignment-1/app.js
@@ -1,6 +1,9 @@
 // required modules used in assignment
 const request = require('request');
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+const yargs = require('yargs');
+
+
 
 // creating options variable as object to pass url to request function
 const options = {
@@ -21,7 +24,7 @@ const csvWriter = createCsvWriter({
       {id: 'lang', title: 'LANGUAGE'},
       {id: 'html_url', title: 'Html Url'},
       {id: 'watchers_count', title: 'Watchers Count'},
-      {id: 'stargazers_count stargazers_count', title: 'Stargazers Count'},
+      {id: 'stargazers_count', title: 'Stargazers Count'},
       {id: 'forks_count', title: 'Forks Count'}
     ]
 });
@@ -29,13 +32,15 @@ const csvWriter = createCsvWriter({
 
 // creating callback for request function
 function callback(error, response) {
+console.log('2')
+
   // check if api got response or not
   if (!error && response.statusCode == 200) {
     // defining data variable to store each value in array
     let data = [];
     response.body.items.forEach(item => {
       // check if filter applyed 
-      if (item.language === 'Python' && item.forks_count >= 200 ) {
+      if (item.language === this.argv.language && item.forks_count >= this.argv.forks ) {
         // printing data for viewing
         console.log('----------------------------------');
         console.log('Name: ' + item.name);
@@ -45,7 +50,7 @@ function callback(error, response) {
         console.log('Stargazers Count: ' + item.stargazers_count);
         console.log('Forks Count: ' + item.forks_count);
 
-        if (item.stargazers_count > 2000) {
+        if (item.stargazers_count > 500) {
           //applying filter and storing data in array
           data2dArray = {name: item.name,lang: item.language, description: item.description, html_url: item.html_url, watchers_count: item.watchers_count, stargazers_count: item.stargazers_count, forks_count: item.forks_count}
           data.push(data2dArray)
@@ -71,5 +76,33 @@ function callback(error, response) {
     console.log('API call fail due to: ' + error);
   }
 }
+
+
+
+yargs.command({
+  command: 'filter',
+  describe: 'Add a new filter.',
+  builder: {
+      language: {
+          describe: 'Project language',
+          demandOption: true,
+          type: 'string'
+      },
+      forks: {
+          describe: 'No. of forks',
+          demandOption: true,
+          type: 'number'
+      }
+  },
+  handler(argv){
+request(options, callback.bind({argv}));
+    
+  }
+})
+
+yargs.parse()
+
+
+console.log('1')
 // request for api call
-request(options, callback);
+console.log('3')
